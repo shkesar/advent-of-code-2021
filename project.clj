0365adfd7c36d9ab1dff29d5b386ff0a78c4b9e9
Shubham Keserwani <shubhamkesar@live.com>
2021-12-01 09:33:43 -0300

    description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "advent-of-code-2021: this year's attempt at advent of code"
+  :url "https://github.com/shkesar/advent-of-code-2021"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
